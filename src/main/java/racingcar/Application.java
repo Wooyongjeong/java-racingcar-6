3fcb0d73197116c7259fa428f5f44666e1c1d7cf
wooyongjeong <wooyong.dev@gmail.com>
2023-12-08 10:00:00 +0900

    feat(Application): 프로그램 기능 구현
    - 객체 인스턴스 생성 및 의존성 주입
    - Controller 통해 게임 실행

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,24 @@
 package racingcar;
 
+import racingcar.controller.RacingCarController;
+import racingcar.service.InputService;
+import racingcar.service.OutputService;
+import racingcar.service.RacingCarService;
+import racingcar.view.InputValidator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputValidator inputValidator = new InputValidator();
+        InputView inputView = new InputView(inputValidator);
+        OutputView outputView = new OutputView();
+
+        InputService inputService = new InputService(inputView);
+        OutputService outputService = new OutputService(outputView);
+        RacingCarService racingCarService = new RacingCarService(inputService, outputService);
+
+        RacingCarController racingCarController = new RacingCarController(racingCarService);
+        racingCarController.play();
     }
 }
